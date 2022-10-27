1eee2babc159a125bac58f05c56587c62a46d96d
´Gabriel <Gabrielmarzioli@hotmail.com>
2022-10-26 21:16:55 -0300

    agregar formulario de cliente

--- front/src/Components/Client/Client.tsx
+++ front/src/Components/Client/Client.tsx
@@ -1,16 +1,36 @@
-import React from 'react'; 
-import {Link }from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Formik, Field, ErrorMessage, Form } from "formik";
+import * as Yup from "yup";
 
 function Client() {
   return (
-    <div>
-      <Link to='/'>
+    <>
+      <Link to="/">
         <h2>Home</h2>
       </Link>
-         <h1>Cliente</h1>
-       
-    </div>
-  )
+      <h1>Cliente</h1>
+      <Formik
+        initialValues={{ name: "" }}
+        validationSchema={Yup.object({
+          name: Yup.string().required("El Campo nombre es requerido"),
+        })}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        <Form>
+          <div>
+            <label>Nombre</label>
+            <Field name="name" type="text" />
+
+            <ErrorMessage component="div" name="name" />
+          </div>
+          <button type="submit">Enviar</button>
+        </Form>
+      </Formik>
+    </>
+  );
 }
 
-export default Client
+export default Client;
