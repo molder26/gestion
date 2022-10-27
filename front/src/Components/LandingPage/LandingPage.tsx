491fef06d224466b0199f52fc06f54708e26e5e2
Pablo Szejpiacki <molder26@hotmail.com>
2022-10-27 19:52:02 -0300

    add animation to home

--- front/src/Components/LandingPage/LandingPage.tsx
+++ front/src/Components/LandingPage/LandingPage.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
 
+import styles from "./LandingPage.module.css";
+
 function LandingPage() {
     return (
         <>
             <NavBar />
+            
+                <div className={styles.textCenter}>
+                    <h3 className={styles.animateCharcter}> GESTION</h3>
+                </div>
+        
         </>
     );
 }
